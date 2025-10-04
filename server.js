3608a6660d0c4fdd7615c0dd3a06d9cabceb0fc4
LHLVilar <101405552+LHLVilar@users.noreply.github.com>
2025-10-04 15:19:19 -0300

    Update server.js
    Código primário

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/api');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
